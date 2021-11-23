metallb/metallb-operator

Implement BFDProfile controller and wire up
This commit extends the configmap reconciliation with BFDProfile and we wire up
the BFDProfile controller to invoke it.
It also adds tests to cover the conversion.

Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

controllers/bfdprofile_controller.go
@@ -18,6 +18,8 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -25,33 +27,33 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	metallbiov1alpha1 "github.com/metallb/metallb-operator/api/v1alpha1"
+	"github.com/metallb/metallb-operator/pkg/render"
 )
 
 // BFDProfileReconciler reconciles a BFDProfile object
 type BFDProfileReconciler struct {
 	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	Log       logr.Logger
+	Scheme    *runtime.Scheme
+	Namespace string
 }
 
 //+kubebuilder:rbac:groups=metallb.io,resources=bfdprofiles,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=metallb.io,resources=bfdprofiles/status,verbs=get;update;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the BFDProfile object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.6.4/pkg/reconcile
-func (r *BFDProfileReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("bfdprofile", req.NamespacedName)
+func (r *BFDProfileReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	r.Log.Info(fmt.Sprintf("Starting BFD Profile reconcile loop for %v", req.NamespacedName))
+	defer r.Log.Info(fmt.Sprintf("Finish BFD Profile reconcile loop for %v", req.NamespacedName))
 
-	// your logic here
-
+	err := reconcileConfigMap(ctx, r.Client, r.Log, r.Namespace)
+	if errors.As(err, &render.RenderingFailed{}) {
+		r.Log.Error(err, "configmap rendering failed", "controller", "addresspool")
+		return ctrl.Result{}, nil
+	}
+	if err != nil {
+		r.Log.Error(err, "failed to reconcile configmap", "controller", "addresspool")
+		return ctrl.Result{RequeueAfter: RetryPeriod}, err
+	}
 	return ctrl.Result{}, nil
 }
 

pkg/render/metallb_configmap.go
@@ -21,6 +21,7 @@ type OperatorConfig struct {
 	DataField     string
 	Pools         []metallbv1alpha1.AddressPool
 	Peers         []metallbv1alpha1.BGPPeer
+	BFDProfiles   []metallbv1alpha1.BFDProfile
 }
 
 // Proto holds the protocol we are speaking.
@@ -57,6 +58,10 @@ func metalLBConfig(data *OperatorConfig) (string, error) {
 	res.Peers = make([]peer, len(data.Peers))
 	for i, p := range data.Peers {
 		res.Peers[i] = peerToMetalLB(p)
+	}
+	res.BFDProfiles = make([]bfdProfile, len(data.BFDProfiles))
+	for i, b := range data.BFDProfiles {
+		res.BFDProfiles[i] = bfdProfileToMetalLB(b)
 	}
 	b, err := yaml.Marshal(&res)
 	if err != nil {
@@ -106,6 +111,7 @@ func peerToMetalLB(p metallbv1alpha1.BGPPeer) peer {
 	}
 	res.RouterID = p.Spec.RouterID
 	res.Password = p.Spec.Password
+	res.BFDProfile = p.Spec.BFDProfile
 	res.NodeSelectors = make([]nodeSelector, len(p.Spec.NodeSelectors))
 	for i, s := range p.Spec.NodeSelectors {
 		res.NodeSelectors[i].MatchLabels = make(map[string]string)
@@ -123,5 +129,19 @@ func peerToMetalLB(p metallbv1alpha1.BGPPeer) peer {
 			}
 		}
 	}
+	return res
+}
+
+func bfdProfileToMetalLB(b metallbv1alpha1.BFDProfile) bfdProfile {
+	res := bfdProfile{}
+	res.Name = b.Name
+	res.ReceiveInterval = b.Spec.ReceiveInterval
+	res.TransmitInterval = b.Spec.TransmitInterval
+	res.DetectMultiplier = b.Spec.DetectMultiplier
+	res.EchoReceiveInterval = b.Spec.EchoReceiveInterval
+	res.EchoTransmitInterval = b.Spec.EchoTransmitInterval
+	res.EchoMode = b.Spec.EchoMode
+	res.PassiveMode = b.Spec.PassiveMode
+	res.MinimumTTL = b.Spec.MinimumTTL
 	return res
 }

pkg/render/metallb_configmap_test.go
@@ -183,6 +183,88 @@ func TestRendering(t *testing.T) {
 			ConfigMapName: "config",
 			NameSpace:     "namespace",
 		},
+		"peersBfd": {
+			ConfigMapName: "config",
+			NameSpace:     "namespace",
+			Pools: []metallbv1alpha1.AddressPool{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-addresspool1",
+						Namespace: "namespace",
+					},
+					Spec: metallbv1alpha1.AddressPoolSpec{
+						Protocol: "bgp",
+						Addresses: []string{
+							"1.1.1.1-1.1.1.100",
+						},
+					},
+				},
+			},
+			Peers: []metallbv1alpha1.BGPPeer{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-peer1",
+						Namespace: "namespace",
+					},
+					Spec: metallbv1alpha1.BGPPeerSpec{
+						MyASN:      23,
+						ASN:        24,
+						Address:    "192.168.1.1",
+						SrcAddress: "192.168.1.2",
+						Port:       1234,
+						HoldTime:   time.Second,
+						RouterID:   "abcd",
+						NodeSelectors: []metallbv1alpha1.NodeSelector{
+							{
+								MatchLabels: map[string]string{
+									"foo": "bar",
+								},
+								MatchExpressions: []metallbv1alpha1.MatchExpression{
+
+									{
+
+										Key:      "k1",
+										Operator: "op1",
+										Values:   []string{"val1", "val2", "val3"},
+									},
+								},
+							}, {
+								MatchLabels: map[string]string{
+									"foo1": "bar1",
+								},
+							},
+						},
+						Password:   "topsecret",
+						BFDProfile: "test-profile1",
+					},
+				},
+			},
+			BFDProfiles: []metallbv1alpha1.BFDProfile{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-profile1",
+						Namespace: "namespace",
+					},
+					Spec: metallbv1alpha1.BFDProfileSpec{
+						ReceiveInterval:      uint32Ptr(12),
+						TransmitInterval:     uint32Ptr(13),
+						DetectMultiplier:     uint32Ptr(14),
+						EchoReceiveInterval:  pointer.StringPtr("45"),
+						EchoTransmitInterval: uint32Ptr(15),
+						EchoMode:             pointer.BoolPtr(true),
+						PassiveMode:          pointer.BoolPtr(true),
+						MinimumTTL:           uint32Ptr(16),
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-profile2",
+						Namespace: "namespace",
+					},
+					Spec: metallbv1alpha1.BFDProfileSpec{},
+				},
+			},
+		},
 	}
 
 	for name, data := range tests {
@@ -249,4 +331,8 @@ func updateGolden(t *testing.T, configFile, goldenFile string) {
 
 func renderedNames(t *testing.T) (string, string) {
 	return filepath.Join("testdata", filepath.FromSlash(t.Name())), filepath.Join("testdata", filepath.FromSlash(t.Name())+".golden")
+}
+
+func uint32Ptr(n uint32) *uint32 {
+	return &n
 }

sabinaaledort/metallboperator

controllers/bfdprofile_controller.go
@@ -18,6 +18,8 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -25,33 +27,33 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	metallbiov1alpha1 "github.com/metallb/metallb-operator/api/v1alpha1"
+	"github.com/metallb/metallb-operator/pkg/render"
 )
 
 // BFDProfileReconciler reconciles a BFDProfile object
 type BFDProfileReconciler struct {
 	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	Log       logr.Logger
+	Scheme    *runtime.Scheme
+	Namespace string
 }
 
 //+kubebuilder:rbac:groups=metallb.io,resources=bfdprofiles,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=metallb.io,resources=bfdprofiles/status,verbs=get;update;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the BFDProfile object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.6.4/pkg/reconcile
-func (r *BFDProfileReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("bfdprofile", req.NamespacedName)
+func (r *BFDProfileReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	r.Log.Info(fmt.Sprintf("Starting BFD Profile reconcile loop for %v", req.NamespacedName))
+	defer r.Log.Info(fmt.Sprintf("Finish BFD Profile reconcile loop for %v", req.NamespacedName))
 
-	// your logic here
-
+	err := reconcileConfigMap(ctx, r.Client, r.Log, r.Namespace)
+	if errors.As(err, &render.RenderingFailed{}) {
+		r.Log.Error(err, "configmap rendering failed", "controller", "bfdprofile")
+		return ctrl.Result{}, nil
+	}
+	if err != nil {
+		r.Log.Error(err, "failed to reconcile configmap", "controller", "bfdprofile")
+		return ctrl.Result{RequeueAfter: RetryPeriod}, err
+	}
 	return ctrl.Result{}, nil
 }
 

pkg/render/metallb_configmap.go
@@ -21,6 +21,7 @@ type OperatorConfig struct {
 	DataField     string
 	Pools         []metallbv1alpha1.AddressPool
 	Peers         []metallbv1alpha1.BGPPeer
+	BFDProfiles   []metallbv1alpha1.BFDProfile
 }
 
 // Proto holds the protocol we are speaking.
@@ -57,6 +58,10 @@ func metalLBConfig(data *OperatorConfig) (string, error) {
 	res.Peers = make([]peer, len(data.Peers))
 	for i, p := range data.Peers {
 		res.Peers[i] = peerToMetalLB(p)
+	}
+	res.BFDProfiles = make([]bfdProfile, len(data.BFDProfiles))
+	for i, b := range data.BFDProfiles {
+		res.BFDProfiles[i] = bfdProfileToMetalLB(b)
 	}
 	b, err := yaml.Marshal(&res)
 	if err != nil {
@@ -109,6 +114,7 @@ func peerToMetalLB(p metallbv1alpha1.BGPPeer) peer {
 	}
 	res.RouterID = p.Spec.RouterID
 	res.Password = p.Spec.Password
+	res.BFDProfile = p.Spec.BFDProfile
 	res.NodeSelectors = make([]nodeSelector, len(p.Spec.NodeSelectors))
 	for i, s := range p.Spec.NodeSelectors {
 		res.NodeSelectors[i].MatchLabels = make(map[string]string)
@@ -126,5 +132,18 @@ func peerToMetalLB(p metallbv1alpha1.BGPPeer) peer {
 			}
 		}
 	}
+	return res
+}
+
+func bfdProfileToMetalLB(b metallbv1alpha1.BFDProfile) bfdProfile {
+	res := bfdProfile{}
+	res.Name = b.Name
+	res.ReceiveInterval = b.Spec.ReceiveInterval
+	res.TransmitInterval = b.Spec.TransmitInterval
+	res.DetectMultiplier = b.Spec.DetectMultiplier
+	res.EchoInterval = b.Spec.EchoInterval
+	res.EchoMode = b.Spec.EchoMode
+	res.PassiveMode = b.Spec.PassiveMode
+	res.MinimumTTL = b.Spec.MinimumTTL
 	return res
 }

pkg/render/metallb_configmap_test.go
@@ -185,6 +185,87 @@ func TestRendering(t *testing.T) {
 			ConfigMapName: "config",
 			NameSpace:     "namespace",
 		},
+		"peersBfd": {
+			ConfigMapName: "config",
+			NameSpace:     "namespace",
+			Pools: []metallbv1alpha1.AddressPool{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-addresspool1",
+						Namespace: "namespace",
+					},
+					Spec: metallbv1alpha1.AddressPoolSpec{
+						Protocol: "bgp",
+						Addresses: []string{
+							"1.1.1.1-1.1.1.100",
+						},
+					},
+				},
+			},
+			Peers: []metallbv1alpha1.BGPPeer{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-peer1",
+						Namespace: "namespace",
+					},
+					Spec: metallbv1alpha1.BGPPeerSpec{
+						MyASN:      23,
+						ASN:        24,
+						Address:    "192.168.1.1",
+						SrcAddress: "192.168.1.2",
+						Port:       1234,
+						HoldTime:   time.Second,
+						RouterID:   "abcd",
+						NodeSelectors: []metallbv1alpha1.NodeSelector{
+							{
+								MatchLabels: map[string]string{
+									"foo": "bar",
+								},
+								MatchExpressions: []metallbv1alpha1.MatchExpression{
+
+									{
+
+										Key:      "k1",
+										Operator: "op1",
+										Values:   []string{"val1", "val2", "val3"},
+									},
+								},
+							}, {
+								MatchLabels: map[string]string{
+									"foo1": "bar1",
+								},
+							},
+						},
+						Password:   "topsecret",
+						BFDProfile: "test-profile1",
+					},
+				},
+			},
+			BFDProfiles: []metallbv1alpha1.BFDProfile{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-profile1",
+						Namespace: "namespace",
+					},
+					Spec: metallbv1alpha1.BFDProfileSpec{
+						ReceiveInterval:  uint32Ptr(12),
+						TransmitInterval: uint32Ptr(13),
+						DetectMultiplier: uint32Ptr(14),
+						EchoInterval:     uint32Ptr(15),
+						EchoMode:         pointer.BoolPtr(true),
+						PassiveMode:      pointer.BoolPtr(true),
+						MinimumTTL:       uint32Ptr(16),
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-profile2",
+						Namespace: "namespace",
+					},
+					Spec: metallbv1alpha1.BFDProfileSpec{},
+				},
+			},
+		},
 	}
 
 	for name, data := range tests {
@@ -251,4 +332,8 @@ func updateGolden(t *testing.T, configFile, goldenFile string) {
 
 func renderedNames(t *testing.T) (string, string) {
 	return filepath.Join("testdata", filepath.FromSlash(t.Name())), filepath.Join("testdata", filepath.FromSlash(t.Name())+".golden")
+}
+
+func uint32Ptr(n uint32) *uint32 {
+	return &n
 }